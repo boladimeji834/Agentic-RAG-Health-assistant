a7e1e595a5b3db14c62c9e1a84c67092c6ec1731
Oladimeji Balogun <115439960+boladimeji834@users.noreply.github.com>
2025-01-10 06:32:10 -1100

    Update process.py

--- deploy/process.py
+++ deploy/process.py
@@ -5,6 +5,7 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 api_key = st.secrets["API_KEY"]
